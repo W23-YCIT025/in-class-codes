84b953ff3ae221d9acdf33c3031a3ae30337d4c8
Jason Jafari <me@jasonjafari.com>
2023-04-04 19:21:42 -0400

    Challenge 009

---
+++ session07/challenge009.js
@@ -0,0 +1,13 @@
+const first = NaN;
+const second = NaN;
+
+console.log('first == second', first == second);
+console.log('first === second', first === second);
+console.log('first > second', first > second);
+console.log('first < second', first < second);
+console.log('second > first', second > first);
+console.log('second >= first', second >= first);
+console.log('second !== first', second !== first);
+console.log('second != first', second != first);
+console.log('first.toString() == second.toString()', first.toString() == second.toString());
+console.log('first.toString() === second.toString()', first.toString() === second.toString());
