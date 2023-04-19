279c54a63737a4a8dc4661bb1385e983eca5649a
Jason Jafari <me@jasonjafari.com>
2023-04-18 20:36:48 -0400

    Misc

---
+++ ycit026/session05/String_vs_toString.js
@@ -0,0 +1,59 @@
+const va1 = 123;
+const va2 = '123';
+const va3 = [1,2,3];
+const va4 = ['1','2','3'];
+const va5 = {};
+const va6 = [];
+const va7 = undefined;
+const va8 = true;
+const va9 = null;
+const va10 = '';
+
+console.log('String(va1)', String(va1));
+console.log('String(va2)', String(va2));
+console.log('String(va3)', String(va3));
+console.log('String(va4)', String(va4));
+console.log('String(va5)', String(va5));
+console.log('String(va6)', String(va6));
+console.log('String(va7)', String(va7));
+console.log('String(va8)', String(va8));
+console.log('String(va9)', String(va9));
+console.log('String(va10)', String(va10));
+
+
+console.log('va1.toString()', va1.toString());
+console.log('va2.toString()', va2.toString());
+console.log('va3.toString()', va3.toString());
+console.log('va4.toString()', va4.toString());
+console.log('va5.toString()', va5.toString());
+console.log('va6.toString()', va6.toString());
+// console.log('va7.toString()', va7.toString());
+console.log('va8.toString()', va8.toString());
+// console.log('va9.toString()', va9.toString());
+console.log('va10.toString()', va10.toString());
+
+
+console.log('Number(va1)', Number(va1));
+console.log('Number(va2)', Number(va2));
+console.log('Number(va3)', Number(va3));
+console.log('Number(va4)', Number(va4));
+console.log('Number(va5)', Number(va5));
+console.log('Number(va6)', Number(va6));
+console.log('Number(va7)', Number(va7));
+console.log('Number(va8)', Number(va8));
+console.log('Number(va9)', Number(va9));
+console.log('Number(va10)', Number(va10));
+
+
+console.log('+va1', +va1);
+console.log('+va2', +va2);
+console.log('+va3', +va3);
+console.log('+va4', +va4);
+console.log('+va5', +va5);
+console.log('+va6', +va6);
+console.log('+va7', +va7);
+console.log('+va8', +va8);
+console.log('+va9', +va9);
+console.log('+va10', +va10);
+
+
